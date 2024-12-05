70eacc7815194db40be43a3c3891d6f052d5d795
Evgenii Bykovskii <bykovsky.ssau@gmail.com>
2024-12-05 19:00:39 +0400

    rename salary_statictics function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pprint import pprint
 from hh_api_tools import (fetch_hh_vacancies,
                           get_vacancy_count_by_text,
                           get_vacancy_count_by_profession_plus_keyword,
-                          fetch_hh__salary_statictics)
+                          get_vacancy_salary_statictics)
 from constants import popular_programming_languages
 
 
@@ -14,7 +14,7 @@ def main():
     # pprint(get_vacancy_count_by_profession_plus_keyword(
     #     profession='Программист',
     #     profession_keywords=popular_programming_languages))
-    pprint(fetch_hh__salary_statictics(text='Python'))
+    pprint(get_vacancy_salary_statictics(text='Python'))
 
 
 if __name__ == '__main__':
