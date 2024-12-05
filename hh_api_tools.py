525d937da66de09535f353f579f10d48a6c5d072
Evgenii Bykovskii <bykovsky.ssau@gmail.com>
2024-12-05 22:57:37 +0400

    fix predict_rub_salary

--- hh_api_tools.py
+++ hh_api_tools.py
@@ -30,3 +30,18 @@ def get_vacancy_count_by_profession_plus_keyword(profession,
 def get_vacancy_salary_statictics(text):
     vacancies = fetch_hh_vacancies(text=text).get('items', 0)
     return [vacancy.get('salary') for vacancy in vacancies]
+
+
+def predict_rub_salary(salary):
+    if salary is None:
+        return None
+    salary_from = salary.get('from')
+    salary_to = salary.get('to')
+    if salary['currency'] != 'RUR':
+        return None
+    if salary_from is None and salary_to is not None:
+        return salary_to * 0.8
+    elif salary_from is not None and salary_to is None:
+        return salary_from * 1.2
+    else:
+        return (salary_from + salary_to) / 2
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pprint import pprint
 from hh_api_tools import (fetch_hh_vacancies,
                           get_vacancy_count_by_text,
                           get_vacancy_count_by_profession_plus_keyword,
-                          get_vacancy_salary_statictics)
+                          get_vacancy_salary_statictics, predict_rub_salary)
 from constants import popular_programming_languages
 
 
@@ -14,7 +14,12 @@ def main():
     # pprint(get_vacancy_count_by_profession_plus_keyword(
     #     profession='Программист',
     #     profession_keywords=popular_programming_languages))
-    pprint(get_vacancy_salary_statictics(text='Python'))
+    # pprint(get_vacancy_salary_statictics(text='Python'))
+    # vacancy = fetch_hh_vacancies(text='Python разработчик')['items'][1]
+    # print(vacancy)
+    python_vacancies_salary = get_vacancy_salary_statictics(text='Python')
+    for python_vacancy_salary in python_vacancies_salary:
+        pprint(predict_rub_salary(python_vacancy_salary))
 
 
 if __name__ == '__main__':
