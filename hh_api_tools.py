30f7f78063bd34bc78e433f9bef21443844ecf3e
Evgenii Bykovskii <bykovsky.ssau@gmail.com>
2024-12-10 09:16:04 +0400

    add get_hh_vacancy_salary_statictics

--- hh_api_tools.py
+++ hh_api_tools.py
@@ -1,57 +1,66 @@
 import statistics
-from collections import OrderedDict
+import time
+from itertools import count
+from requests import HTTPError
 
 from requests_tools import get_response
-from datetime_tools import get_date_offset_by_days
 
 
-def fetch_hh_vacancies(professional_role=None, area=None, date_from=None,
-                       text=None):
+def fetch_hh_all_vacancies(**kwargs):
     url = 'https://api.hh.ru/vacancies'
-    params = {
-        'professional_role': professional_role,
-        'area': area,
-        'date_from': get_date_offset_by_days(date_from) if date_from else None,
-        'text': text
-    }
-    return get_response(url, params).json()
+    params = {**kwargs}
+    for page in count():
+        params['page'] = page
+        try:
+            response = get_response(url, params=params)
+        except HTTPError as http_err:
+            if http_err.response.status_code == 502:
+                print(
+                    f"Ошибка 502 на странице {page}. Пропускаем страницу.")
+                time.sleep(1)
+                continue
+            elif http_err.response.status_code == 403:
+                print(f"Доступ запрещен: {http_err}")
+            else:
+                print(f"HTTP ошибка: {http_err}")
+                break
+        except Exception as err:
+            print(f"Ошибка: {err}")
+            break
+
+        page_payload = response.json()
+        print(f"Обработка страницы {page} из {page_payload['pages'] - 1}")
+
+        yield page_payload
+        if page >= page_payload['pages'] - 1:
+            break
+        time.sleep(1)
 
 
-def get_vacancy_count_by_text(text):
-    return fetch_hh_vacancies(text=text).get('found', 0)
-
-
-def get_vacancy_count_by_profession_plus_keyword(profession,
-                                                 profession_keywords):
+def get_hh_vacancy_salary_statictics(profession,
+                                     profession_keywords):
     stats = {}
-    for keyword in profession_keywords:
-        stats[keyword] = get_vacancy_count_by_text(
-            text=f'{profession} {keyword}')
-    return stats
-
-
-def get_vacancy_salary_statictics_by_profession_plus_keyword(profession,
-                                                             profession_keywords):
-    stats = {}
 
     for keyword in profession_keywords:
-        salaries = [predict_rub_salary(python_vacancy_salary)
-                    for python_vacancy_salary in get_vacancy_salary_statictics(
-                text=f'{profession} {keyword}')
-                    if predict_rub_salary(python_vacancy_salary) is not None]
+        all_pages = list(
+            fetch_hh_all_vacancies(text=f'{profession} {keyword}'))
+
+        vacancies_found = sum(page.get('found', 0) for page in all_pages)
+        salaries = [
+            salary for page in all_pages
+            for vacancy in page.get('items', [])
+            if
+            (salary := predict_rub_salary(vacancy.get('salary'))) is not None
+        ]
+        average_salary = int(statistics.mean(salaries)) if salaries else 0
+
         stats[keyword] = {
-            "vacancies_found": get_vacancy_count_by_text(
-                text=f'{profession} {keyword}'),
+            "vacancies_found": vacancies_found,
             "vacancies_processed": len(salaries),
-            "average_salary": int(
-                statistics.mean(salaries)) if salaries else 0,
+            "average_salary": average_salary,
         }
+
     return stats
-
-
-def get_vacancy_salary_statictics(text):
-    vacancies = fetch_hh_vacancies(text=text).get('items', 0)
-    return [vacancy.get('salary') for vacancy in vacancies]
 
 
 def predict_rub_salary(salary):
--- main.py
+++ main.py
@@ -1,28 +1,11 @@
 from pprint import pprint
 
-from hh_api_tools import (fetch_hh_vacancies,
-                          get_vacancy_count_by_text,
-                          get_vacancy_count_by_profession_plus_keyword,
-                          get_vacancy_salary_statictics, predict_rub_salary,
-                          get_vacancy_salary_statictics_by_profession_plus_keyword)
+from hh_api_tools import get_hh_vacancy_salary_statictics
 from constants import popular_programming_languages
 
 
 def main():
-    # print(fetch_hh_vacancies(professional_role=96, area=1, date_from=30))
-    # print(fetch_hh_vacancies(professional_role=96, area=1))
-    # print(get_vacancy_count_by_text(text='Программист Python'))
-    # pprint(get_vacancy_count_by_profession_plus_keyword(
-    #     profession='Программист',
-    #     profession_keywords=popular_programming_languages))
-    # pprint(get_vacancy_salary_statictics(text='Python'))
-    # vacancy = fetch_hh_vacancies(text='Python разработчик')['items'][1]
-    # print(vacancy)
-    # python_vacancies_salary = get_vacancy_salary_statictics(
-    #     text='Программист Python')
-    # for python_vacancy_salary in python_vacancies_salary:
-    #     pprint(predict_rub_salary(python_vacancy_salary))
-    pprint(get_vacancy_salary_statictics_by_profession_plus_keyword(
+    pprint(get_hh_vacancy_salary_statictics(
         profession='Программист',
         profession_keywords=popular_programming_languages))
 
